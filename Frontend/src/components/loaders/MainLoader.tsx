ed05dcd321746bf94e827f77ac094b6ab97a4346
Anantha krishnan <ananthakrishhh2002@gmail.com>
2024-08-09 09:48:48 +0530

    loader correction

--- Frontend/src/components/loaders/MainLoader.tsx
+++ Frontend/src/components/loaders/MainLoader.tsx
@@ -3,7 +3,7 @@ import { RiLoaderLine } from "react-icons/ri";
 const MainLoader = () => {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <RiLoaderLine color="#0067FF"/>
+      <RiLoaderLine color="#0067FF" size={50}/>
     </div>
   )
 }
